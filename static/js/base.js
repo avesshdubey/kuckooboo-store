7c949de420ac5c1e3971c77f264ed73419a17142
avesshdubey <aveshdubey648@gmail.com>
2026-03-04 18:44:43 +0530

    new base.html

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,14 @@
-// Basic future-ready JS file
-
 document.addEventListener("DOMContentLoaded", function () {
+
     console.log("Brand theme loaded.");
+
+    const menuToggle = document.getElementById("menuToggle");
+    const navLinks = document.getElementById("navLinks");
+
+    if (menuToggle) {
+        menuToggle.addEventListener("click", function () {
+            navLinks.classList.toggle("active");
+        });
+    }
+
 });
